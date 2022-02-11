fbb190143ffca7cfd1519eb8043885d978f67243
19-39353-1@student.aiub.edu <19-39353-1@student.aiub.edu>
2022-02-12 01:00:52 +0600

    class Task

---
+++ Classtask1/Classtask1/Controllers/MYCVController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Classtask1.Controllers
+{
+    public class MYCVController : Controller
+    {
+        // GET: MYCV
+        public ActionResult MYCV()
+        {
+            return View();
+        }
+    }
+}
